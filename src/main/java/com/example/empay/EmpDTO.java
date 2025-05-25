4597f5455902d5a0c62555415c54f4d237e7ee21
Vishal Umasankar <vishalumasankar@gmail.com>
2025-05-25 17:11:29 +0530

    Added UC7 Methods

--- src/main/java/com/example/empay/EmpDTO.java
+++ src/main/java/com/example/empay/EmpDTO.java
@@ -1,10 +1,17 @@
 package com.example.empay;
 
-import java.time.LocalDate; // <-- ADDED THIS IMPORT for LocalDate
+import java.time.LocalDate;
 import java.util.List;
+
 import jakarta.validation.constraints.Min;
-import jakarta.validation.constraints.Pattern;
-import lombok.ToString;
+import jakarta.validation.constraints.NotBlank; 
+import jakarta.validation.constraints.NotNull; 
+import jakarta.validation.constraints.PastOrPresent;
+import jakarta.validation.constraints.Pattern; 
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+import lombok.ToString; 
 
 @ToString
 public class EmpDTO {
@@ -17,13 +24,21 @@ public class EmpDTO {
          message = "Min Wage should be more than 500")
     public long salary;
 
+    @Pattern(regexp = "male|female", 
+             message = "Gender needs to be male or female")
     public String gender;
 
-    public LocalDate startDate; 
+    @JsonFormat(pattern = "dd MMM yyyy") 
+    @NotNull(message = "startDate should Not Be Empty") 
+    @PastOrPresent(message = "startDate should be past or todays date") 
+    public LocalDate startDate;
 
+    @NotBlank(message = "Note cannot be Empty") 
     public String note;
 
+    @NotBlank(message = "profilePic cannot be Empty") 
     public String profilePic;
 
+    @NotNull(message = "department should Not Be Empty") 
     public List<String> department;
 }
